6e1ff61f82d822f9020b80ea0c1bd02fd472cabe
VoVanThuan2004 <vot04766@gmail.com>
2026-06-16 13:26:40 +0700

    Update lombok version

--- src/main/java/com/example/restaurant_food_system/dto/response/ApiResponse.java
+++ src/main/java/com/example/restaurant_food_system/dto/response/ApiResponse.java
@@ -1,12 +1,11 @@
 package com.example.restaurant_food_system.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter @Setter
 @Builder
+@AllArgsConstructor @NoArgsConstructor
 public class ApiResponse<T> {
     private String status;
     private int code;
